16ad528dc124bfb7b52e1b558709a6ac34fc701a
sam lin <evaoosyz@gmail.com>
2016-08-21 15:51:16 +0800

    加入 AutoMapper4

---
+++ AutoMapper4Demo/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutoMapper4Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<User, UserViewModel>();
+            });
+            var mapper = config.CreateMapper();
+
+            User sam = new User()
+            {
+                UserID = 1,
+                Name = "sam"
+            };
+
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            for (int i = 0; i < 1000000; i++)
+            {
+                UserViewModel model = mapper.Map<UserViewModel>(sam);
+            }
+            watch.Stop();
+            ViewBag.Milliseconds = watch.ElapsedMilliseconds;
+            return View();
+        }
+    }
+
+    public class UserViewModel
+    {
+        public int UserID { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class User
+    {
+        public int UserID { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ AutoMapper4Demo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AutoMapper4Demo.Startup))]
+namespace AutoMapper4Demo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
